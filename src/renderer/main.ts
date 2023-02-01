8c73f502c36afaf2a75c541a03da9a4595da615b
hank <hank-c@qq.com>
2023-02-01 16:00:49 +0800

    feat: add sqlite3

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -5,3 +5,6 @@ import App from "./App.vue";
 import { router } from "./router";
 import { createPinia } from "pinia";
 createApp(App).use(createPinia()).use(router).mount("#app");
+
+const Database = require("better-sqlite3");
+const db = new Database("db.db", { verbose: console.log, nativeBinding: "./node_modules/better-sqlite3/build/Release/better_sqlite3.node" });
