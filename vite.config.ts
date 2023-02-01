dc89d0f5902c1ec86014dd79608e57be589ec71c
hank <hank-c@qq.com>
2023-02-01 12:28:09 +0800

    feat: init electorn vite vue

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { devPlugin } from "./plugins/devPlugin";
+
+export default defineConfig({
+  plugins: [devPlugin(), vue()],
+});
