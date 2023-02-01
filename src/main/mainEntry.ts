35ef10b75ee936245fdd3831eaeca324453db7a4
hank <hank-c@qq.com>
2023-02-01 14:26:46 +0800

    feat: add vue-router

--- src/main/mainEntry.ts
+++ src/main/mainEntry.ts
@@ -1,10 +1,15 @@
-import { app, BrowserWindow } from "electron";
+import { app, autoUpdater, BrowserWindow } from "electron";
 import { CustomScheme } from "./CustomScheme";
+import { Updater } from "./Updater";
+import { CommonWindowEvent } from "./CommonWindowEvent";
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = "true";
+app.on("browser-window-created", (e, win) => {
+  CommonWindowEvent.regWinEvent(win);
+});
 let mainWindow: BrowserWindow;
-
 app.whenReady().then(() => {
   let config = {
+    frame: false,
     webPreferences: {
       nodeIntegration: true,
       webSecurity: false,
@@ -15,11 +20,17 @@ app.whenReady().then(() => {
       disableHtmlFullscreenWindowResize: true,
     },
   };
+
   mainWindow = new BrowserWindow(config);
+
+  mainWindow.webContents.openDevTools({ mode: "undocked" });
   if (process.argv[2]) {
     mainWindow.loadURL(process.argv[2]);
   } else {
     CustomScheme.registerScheme();
     mainWindow.loadURL(`app://index.html`);
+    // Updater.check();
   }
+  CommonWindowEvent.listen();
+  CommonWindowEvent.regWinEvent(mainWindow);
 });
